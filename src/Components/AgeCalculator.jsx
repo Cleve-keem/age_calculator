fa9ce06e46bdf416327884effc789f5ac68a0a52
hakeem <belloharyourmidey@gmail.com>
2024-10-11 11:00:32 -0700

    third  commit

--- src/Components/AgeCalculator.jsx
+++ src/Components/AgeCalculator.jsx
@@ -1,4 +1,5 @@
 import './AgeCalculator.modulus.css';
+// import 'remixicon/fonts/remixicon.css';
 
 function AgeCalculator() {
     return (
@@ -9,28 +10,33 @@ function AgeCalculator() {
                         <div className="input">
                             <label htmlFor="day">Day</label>
                             <input type="number" name="day" id="day"/>
+                            <p className='error'></p>
                         </div>
                         <div className="input">
                             <label htmlFor="month">Month</label>
                             <input type="number" name="month" id="month"/>
+                            <p className='error'></p>
                         </div>
                         <div className="input">
                             <label htmlFor="year">Years</label>
                             <input type="number" name="year" id="year"/>
+                            <p className='error'></p>
                         </div>
                     </div>
-                    <button className="submit"><i className="ri-arrow-down-line"></i></button>
+                    <button className="submit">
+                        <i className="ri-arrow-down-line arrow-down"></i>
+                    </button>
                 </div>
                 <hr />
                 <div className="outputs">
                     <div className="output">
-                        <div className="years">-- <span>Years</span></div>
+                        <div className="years">- - <span className='year-span'>Years</span></div>
                     </div>
                     <div className="output">
-                        <div className="months">-- <span>Months</span></div>
+                        <div className="months">- - <span className='month-span'>Months</span></div>
                     </div>
                     <div className="output">
-                        <div className="days">-- <span>Days</span></div>
+                        <div className="days"> - - <span className='day-span'>Days</span></div>
                     </div>
                 </div>
             </div>
