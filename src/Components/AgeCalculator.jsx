ca345f1b72b6141ec8c22759cd6435dc98908474
hakeem <belloharyourmidey@gmail.com>
2024-10-12 12:52:11 -0700

    adjusting the codes

--- src/Components/AgeCalculator.jsx
+++ src/Components/AgeCalculator.jsx
@@ -5,88 +5,120 @@ function AgeCalculator() {
     const [day, setDay] = useState('');
     const [month, setMonth] = useState('');
     const [year, setYear] = useState('');
-    const [errorDay, setErrorDay] = useState('');
-    const [errorMonth, setErrorMonth] = useState('');
-    const [errorYear, setErrorYear] = useState('');
+    const [error, setError] = useState({ day: "", month: "", year: ""});
     const [dayOutput, setDayOutput] = useState('- -');
     const [monthOutput, setMonthOutput] = useState('- -');
     const [yearOutput, setYearOutput] = useState('- -');
-
-    let isValid = true;
-
-    const handleDayChange = (e) => {
-        const value = e.target.value;
-        setDay(value);
-        if (value < 1 || value > 31) {
-            setErrorDay("Invalid day");
-            isValid = false;
-        } else {
-            setErrorDay('');
-        }
-    };
-
-    const handleMonthChange = (e) => {
-        const value = e.target.value;
-        setMonth(value);
-        if (value < 1 || value > 12) {
-            setErrorMonth("Invalid month");
-            isValid = false;
-        } else {
-            setErrorMonth('');
-        }
-    };
     
     const currentYear = new Date().getFullYear();
-    const handleYearChange = (e) => {
-        const value = e.target.value;
-        setYear(value);
-        if (value < 1970 || value > currentYear) {
-            setErrorYear(`Year 1970-${currentYear}`);
+
+    const validateInputs = () => {
+        let isValid = true;
+        const newErrors = { day: "", month: "", year: ""};
+        if(!day || day < 1 || day > 31){
             isValid = false;
-        } else {
-            setErrorYear('');
+            newErrors.day = "Invalid Date";
         }
-    };
+
+        if(!month || month < 0 || month > 12){
+            isValid = false;
+            newErrors.month = "Invalid Month";
+        }
+
+        if(!year || year < 1900 || month > currentYear){
+            isValid = false;
+            newErrors.month = "Invalid Month";
+        }
+
+        setError(newErrors);
+        return isValid;
+    }
 
     const calculateAge = () => {
-        isValid = true;
-
-        if (errorDay || errorMonth || errorYear) {
-            isValid = false;
-        }
-
-        const birthday = new Date(`${year}-${month}-${day}`);
-        if (isNaN(birthday)) {
-            setErrorDay("Invalid date");
-            isValid = false;
-        }
-
-        if (isValid) {
-            const today = new Date();
-            let ageYear = today.getFullYear() - birthday.getFullYear();
-            let ageMonth = today.getMonth() - birthday.getMonth();
-            let ageDay = today.getDate() - birthday.getDate();
-
-            if (ageMonth < 0 || (ageMonth === 0 && ageDay < 0)) {
-                ageYear--;
-                ageMonth += 12;
-            }
-
-            if (ageDay < 0) {
-                const prevMonthDays = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
-                ageDay += prevMonthDays;
-                ageMonth--;
-            }
-
-            setDayOutput(ageDay);
-            setMonthOutput(ageMonth);
-            setYearOutput(ageYear);
-        } else {
+        if(!validateInputs()){
+            // set all the output back to default
             setDayOutput('- -');
             setMonthOutput('- -');
             setYearOutput('- -');
+
+            return;
         }
-    };
+
+        const birthday = new Date(`${year}-${month}-${day}`);
+    }
+
+    // const handleDayChange = (e) => {
+    //     const value = e.target.value;
+    //     setDay(value);
+    //     if (value < 1 || value > 31) {
+    //         setErrorDay("Invalid day");
+    //         isValid = false;
+    //     } else {
+    //         setErrorDay('');
+    //     }
+    // };
+
+    // const handleMonthChange = (e) => {
+    //     const value = e.target.value;
+    //     setMonth(value);
+    //     if (value < 1 || value > 12) {
+    //         setErrorMonth("Invalid month");
+    //         isValid = false;
+    //     } else {
+    //         setErrorMonth('');
+    //     }
+    // };
+    
+    // const handleYearChange = (e) => {
+    //     const value = e.target.value;
+    //     setYear(value);
+    //     if (value < 1970 || value > currentYear) {
+    //         setErrorYear(`Year 1970-${currentYear}`);
+    //         isValid = false;
+    //     } else {
+    //         setErrorYear('');
+    //     }
+    // };
+
+    // const calculateAge = () => {
+    //     isValid = true;
+
+    //     if (errorDay || errorMonth || errorYear) {
+    //         isValid = false;
+    //     }
+
+    //     const birthday = new Date(`${year}-${month}-${day}`);
+    //     if (isNaN(birthday)) {
+    //         setErrorDay("Invalid date");
+    //         isValid = false;
+    //     }
+
+    //     if (isValid) {
+    //         const today = new Date();
+    //         let ageYear = today.getFullYear() - birthday.getFullYear();
+    //         let ageMonth = today.getMonth() - birthday.getMonth();
+    //         let ageDay = today.getDate() - birthday.getDate();
+
+    //         if (ageMonth < 0 || (ageMonth === 0 && ageDay < 0)) {
+    //             ageYear--;
+    //             ageMonth += 12;
+    //         }
+
+    //         if (ageDay < 0) {
+    //             const prevMonthDays = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    //             ageDay += prevMonthDays;
+    //             ageMonth--;
+    //         }
+
+    //         setDayOutput(ageDay);
+    //         setMonthOutput(ageMonth);
+    //         setYearOutput(ageYear);
+    //     } else {
+    //         setDayOutput('- -');
+    //         setMonthOutput('- -');
+    //         setYearOutput('- -');
+    //     }
+    // };
 
     return (
         <div className="container">
